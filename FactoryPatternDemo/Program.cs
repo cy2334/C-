a157992016db139fc347ff372ad707512a34d05a
cy2334 <1178849864@qq.com>
2025-05-11 10:06:19 +0800

    抽象工厂补交

---
+++ FactoryPatternDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FactoryPatternDemo;
+
+IPayment payment = PaymentFactory.CreatePayment("alipay");
+payment.Pay(100.50m);
+
+payment= PaymentFactory.CreatePayment("wechat");
+payment.Pay(200.00m);
+Console.WriteLine("Hello, World!");
+
+IPaymentFactory factory = new AlipayFactory();
+IPayment payment2 = factory.CreatePayment();
+payment.Pay(100.50m);
+
+factory = new WechatPayFactory();
+payment2 = factory.CreatePayment();
+payment2.Pay(200.00m);
